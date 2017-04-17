63596dc72b311332926a2d8fec3473ac0683eb24
Ben McCain <mccain.ben2@gmail.com>
2017-04-17 16:52:40 -0400

    magnets completed

---
+++ db/models/magnet.js
@@ -0,0 +1,44 @@
+'use strict'
+
+const {STRING, INTEGER, TEXT, ARRAY} = require('sequelize')
+
+module.exports = db => db.define('magnet', {
+  quote: {
+    type: TEXT,
+    allowNull: false
+  },
+  price: {
+    type: INTEGER,
+    allowNull: false
+  },
+  image: {
+    type: STRING,
+    allowNull: false
+  },
+  title: {
+    type: STRING,
+    allowNull: false
+  },
+  description: {
+    type: TEXT,
+    allowNull: false
+  },
+  itemNumber: {
+    type: INTEGER,
+    allowNull: false
+  },
+  size: {
+    type: ARRAY(INTEGER),
+    allowNull: false
+  },
+  mood: {
+    type: ARRAY(STRING),
+    allowNull: false
+  }
+
+})
+
+module.exports.associations = (Magnet, {Speaker, Review}) => {
+  Magnet.belongsTo(Speaker)
+  Magnet.hasMany(Review, {as: 'reviews'})
+}
