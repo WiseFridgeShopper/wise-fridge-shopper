f80d580bf744786bd2d7986bcf348885e8a222c9
Anton Cataldi <antoncataldi@gmail.com>
2017-04-18 12:45:22 -0400

    Add router for magnets

---
+++ server/magnets.js
@@ -0,0 +1,51 @@
+'use strict'
+
+const db = require('APP/db')
+const Magnet = db.model('magnets')
+const reviews = ('APP/server/reviews')
+
+module.exports = require('express').Router()
+  .get('/', (req, res, next) => {
+    Magnet.findAll()
+      .then(magnets => res.json(magnets))
+      .catch(next)
+  })
+  .post('/', (req, res, next) => {
+    Magnet.create(req.body)
+      .then(magnet => res.status(201).json(magnet))
+      .catch(next)
+  })
+  .get('/:id', (req, res, next) => {
+    Magnet.findById(req.params.id)
+    .then(magnet => res.json(magnet))
+    .catch(next)
+  })
+  .delete('/:id', (req, res, next) => {
+    Magnet.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+    .then(destroyedRows => {
+      destroyedRows ? res.sendStatus(204) : res.sendStatus(404)
+    })
+    .catch(next)
+  })
+  .put('/:id', (req, res, next) => {
+    Magnet.update(req.body, {
+      where: {
+        id: req.params.id
+      },
+      returning: true,
+      plain: true
+    })
+    .then(updatedMagnet => {
+      updatedMagnet ? res.send(updatedMagnet) : res.sendStatus(404)
+    })
+    .catch(next)
+  })
+  .use('/:id/reviews', (req, res, next) => {
+    req.queryIdString = 'magnet'
+    req.queryId = req.params.id
+    next()
+  }, reviews)
