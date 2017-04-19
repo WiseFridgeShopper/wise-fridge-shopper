71804b7ffa8f2c3fba3aeb09aafbf572763c8428
Judah Hamaoui <ayhamaoui@gmail.com>
2017-04-19 17:04:04 -0400

    feat: material ui

--- app/components/Login.jsx
+++ app/components/Login.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
+import RaisedButton from 'material-ui/RaisedButton'
+import TextField from 'material-ui/TextField'
 
 export const Login = ({ login }) => (
   <form onSubmit={evt => {
     evt.preventDefault()
     login(evt.target.username.value, evt.target.password.value)
   } }>
-    <input name="username" />
-    <input name="password" type="password" />
-    <input type="submit" value="Login" />
+    <TextField name="username" />
+    <TextField name="password" type="password" />
+    <RaisedButton label="login" type="submit" value="Login" />
   </form>
 )
 
