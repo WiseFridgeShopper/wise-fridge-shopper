1bdb31e65e69c5587dbb5e874c488dcc0230a736
Anton Cataldi <antoncataldi@gmail.com>
2017-04-20 12:36:13 -0400

    added Card, CardHeader, and CardText comps to SingleSpeaker

--- app/components/SingleSpeaker.jsx
+++ app/components/SingleSpeaker.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import {Card, CardHeader, CardText} from 'material-ui/Card'
+import {GridList, GridTile} from 'material-ui/GridList'
 
 const SingleSpeaker = props => {
   return (
-    <div>
-      <h1>Single Speaker Route</h1>
-    </div>
+    <Card>
+      <CardHeader
+        title="Speaker Name"
+        actAsExpander={true}
+        showExpandableButton={true}></CardHeader>
+      <CardText expandable={true}>
+        I am the bio.
+      </CardText>
+    </Card>
   )
 }
 
