8bd9fa7eaf27add151465a3b82bd57d5a1a0b3ea
Judah Hamaoui <ayhamaoui@gmail.com>
2017-04-20 15:06:11 -0400

    feat: shopping cart

--- app/components/Cart.jsx
+++ app/components/Cart.jsx
@@ -1,13 +1,37 @@
 import React from 'react'
 import Drawer from 'material-ui/Drawer'
-import AppBar from 'material-ui/AppBar'
+import CartMenuItem from './CartMenuItem'
 import RaisedButton from 'material-ui/RaisedButton'
 import MenuItem from 'material-ui/MenuItem';
+import AppBar from 'material-ui/AppBar';
+
 
 export default class Cart extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {open: false}
+    this.state = {
+      open: false,
+      totalPrice: 10,
+      cartMagnets: [{
+        quote: 'Many a test has failed',
+        price: 3.95,
+        image: 'cdn.shopify.com/s/files/1/0273/4903/products/ralph-waldo-emerson-fridge-magnet-1_large.jpg?v=1380467104',
+        title: 'Koans about Testing',
+        description: 'Amazing magnet with Koans from testing',
+        itemNumber: 12345,
+        size: [2, 4],
+        mood: ['zany', 'moody']
+      }, {
+        quote: 'Different Quote',
+        price: 5.95,
+        image: 'cdn.shopify.com/s/files/1/0273/4903/products/ralph-waldo-emerson-fridge-magnet-3_large.jpg?v=1380467104',
+        title: 'Magnet info',
+        description: 'Amazing magnet with Koans from testing',
+        itemNumber: 73832,
+        size: [2, 4],
+        mood: ['zany', 'moody']
+      }]
+    }
   }
 
   handleToggle = () => this.setState({open: !this.state.open});
@@ -15,17 +39,6 @@ export default class Cart extends React.Component {
   handleClose = () => this.setState({open: false});
 
   render() {
-    const dummyOrder = {3: '5', 7: '4'}
-    const dummyMagnet = {
-      quote: 'Many a test has failed',
-      price: 3.95,
-      image: 'cdn.shopify.com/s/files/1/0273/4903/products/ralph-waldo-emerson-fridge-magnet-1_large.jpg?v=1380467104',
-      title: 'Koans about Testing',
-      description: 'Amazing magnet with Koans from testing',
-      itemNumber: 12345,
-      size: [2, 4],
-      mood: ['zany', 'moody']
-    }
     return (
       <div>
         <h1>Cart Route</h1>
@@ -35,17 +48,29 @@ export default class Cart extends React.Component {
         />
         <Drawer
           docked={false}
-          width={375}
+          width={700}
           openSecondary={true}
           open={this.state.open}
           onRequestChange={(open) => this.setState({open})}
         >
-          <MenuItem onTouchTap={this.handleClose}>Product 1</MenuItem>
-          <MenuItem onTouchTap={this.handleClose}>Product 2</MenuItem>
-          <img src={`http://${dummyMagnet.image}`} alt="Magnet pic" style="width:304px;height:228px;"/>
-          <MenuItem onTouchTap={this.handleClose}>{dummyOrder[3]}</MenuItem>
+          <AppBar title="Shopping Cart" />
+          <MenuItem>Magnets</MenuItem>
+          {this.state.cartMagnets.map(magnet => {
+            return (
+              <div>
+                <CartMenuItem key={magnet.itemNumber} magnet={magnet}/>
+                <hr/>
+              </div>
+            )
+          })}
+          <RaisedButton
+          label="Checkout"
+          onTouchTap={this.handleToggle}
+        />
         </Drawer>
       </div>
     )
   }
 }
+
+// onTouchTap={this.handleClose}
