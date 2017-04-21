8f72d9afbfce39aaf748fa1a9512c66357fb40d2
Anton Cataldi <antoncataldi@gmail.com>
2017-04-20 20:13:30 -0400

    fix: sizing of SingleMagnet

--- app/components/SingleMagnet.jsx
+++ app/components/SingleMagnet.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import {List, ListItem} from 'material-ui/List'
 import Paper from 'material-ui/Paper'
+import {Card, CardMedia, CardHeader, CardText} from 'material-ui/Card'
+import FlatButton from 'material-ui/FlatButton'
+
 const magnet = {
   title: 'Henry David Thoreau Fridge Magnet #1',
   quote: "What you get by achieving your goals is not as important as what you become by achieving your goals.",
@@ -23,24 +26,37 @@ const styles = {
 }
 
 const SingleMagnet = props => {
+  // when we implement a controlled component for this dumb component, below line show be uncommented
+  // const magnet = props.selectedMagnet
   return (
-    <div>
-      <Paper style={styles.description}>
-        <List>
-          <ListItem ></ListItem>
-          <ListItem primaryText={<h1>magnet.title</h1>} />
-          <ListItem primaryText={`Product description: ${magnet.description}`} />
-          <ListItem primaryText={`Size: ${magnet.size}`} />
-          <ListItem primaryText={`Item #: ${magnet.itemNumber}`} />
-          <ListItem primaryText={`Mood: ${magnet.mood}`} />
-        </List>
-      </Paper>
-      <Paper style={styles.reviews}>
-        <List>
-          <ListItem> 5.0 This is awesome!</ListItem>
-          <ListItem> 0.5 This is shit!</ListItem>
-        </List>
-      </Paper>
+    <div className={'row'}>
+      <div className={'col-md-6'}>
+        <Card>
+          <CardHeader
+            title={magnet.title}
+            actAsExpander={true}
+            >
+          </CardHeader>
+          <CardMedia >
+            <img src={magnet.image} className={'img-responsive'}/>
+          </CardMedia>
+          <CardText >
+            <List>
+              <ListItem ><FlatButton label='buy me' primary={true}></FlatButton></ListItem>
+              <ListItem primaryText={magnet.title} />
+              <ListItem primaryText={`Size: ${magnet.size}`} />
+              <ListItem primaryText={`Item #: ${magnet.itemNumber}`} />
+              <ListItem primaryText={`Mood: ${magnet.mood}`} />
+            </List>
+          </CardText>
+          <CardText expandable={true}>
+            <List>
+              <ListItem> 5.0 This is awesome!</ListItem>
+              <ListItem> 0.5 This is shit!</ListItem>
+            </List>
+          </CardText>
+        </Card>
+      </div>
     </div>
   )
 }
