f8563ef9d5d86b7c17bc8c10a453479c340c1633
zc <zachcaceres@gmail.com>
2017-04-21 16:46:42 -0400

    fix: size magnets to fit gridlist

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -138,6 +138,10 @@ export default class Home extends React.Component {
   *  Change which filter we run to match the tab. || operator can be used inside gridlist to
   *  render proper thumbnails. */
   render() {
+    const style = {
+
+      // minHeight: '300%'
+    }
     return (
       <div>
         <div className='row'>
@@ -147,7 +151,7 @@ export default class Home extends React.Component {
         </div>
         <div className='row'>
           <div className='col-md-12'>
-            <GridList cols={3} title='My Grid'>
+            <GridList style={style} padding={50} cols={4} title='Products' >
               { this.props.selectedTab === 'speakers' ? this.renderFilteredSpeakers() : this.renderFilteredMagnets() }
             </GridList>
           </div>
--- app/components/MagnetThumbnail.jsx
+++ app/components/MagnetThumbnail.jsx
@@ -3,9 +3,14 @@ import {Link} from 'react-router'
 import {GridTile} from 'material-ui/GridList'
 import IconButton from 'material-ui/IconButton' // used for purchase button
 
+const style = {
+  minHeight: '220px',
+  maxHeight: '220px',
+}
+
 const MagnetThumbnail = props => (
     <Link key={props.id} to={`/magnets/${props.id}`}>
-      <GridTile key={props.id} actionIcon={<IconButton children={<i className="material-icons md-light">add shopping cart</i>}></IconButton>}>
+      <GridTile key={props.id} style={style} actionIcon={<IconButton children={<i className="material-icons md-light">add shopping cart</i>}></IconButton>}>
         <img src={`http://${props.image}`}/>
       </GridTile></Link>
     )
