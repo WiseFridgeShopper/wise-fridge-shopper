a97876913e801fcc095f17e96e8e63fc73751da5
Anton Cataldi <antoncataldi@gmail.com>
2017-04-21 14:39:12 -0400

    add skeleton for Review Component

---
+++ app/components/Review.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {Card, CardHeader, CardText} from 'material-ui/Card'
+import Stars from 'material-ui/svg-icons/action/stars'
+
+const Review = (props) => {
+  return (
+    <Card>
+      <CardHeader
+        title="Product Name Here"
+        avatar={<Stars/>}
+        />
+      <CardText>
+        Review for crappy product goes here.
+      </CardText>
+    </Card>
+  )
+}
+
+export default Review
