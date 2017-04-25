5faf576dd1c09769866cbe3b425fd3580a87d252
Ben McCain <mccain.ben2@gmail.com>
2017-04-25 11:07:00 -0400

    checkout

--- app/components/Checkout.jsx
+++ app/components/Checkout.jsx
@@ -1,9 +1,52 @@
 import React from 'react'
+import store from '../store'
+
+const localState = store.getState()
 
 const Checkout = props => {
   return (
     <div>
-      <h1>Checkout Route</h1>
+      <h2 style={{ fontStyle: 'arial' }}>Current Checkout</h2>
+      <ul style={{ listStyle: 'none' }}>
+        {
+          props.orders && props.orders.map(order => {
+            return (
+              <div className="orderHistory">
+              <li>
+                <div key={order.id}>
+                  <h3>{`Order #${order.id}`}</h3>
+                  <span>{`Purchase Date: ${order.created_at}`}</span>
+                  {
+                    localState.magnet && localState.magnet.filter(mag => {
+                      return order.products[mag.itemNumber]
+                    }).map(magnet => {
+                      return (
+                      <div style={{ padding: 5 }}>
+                        <img style={{ width: '35%' }} src={magnet.image}/>
+                        <div>
+                          <span>{magnet.title}</span>
+                        </div>
+                        <div>
+                          <span>{`$${magnet.price}`}</span>
+                        </div>
+                        <div>
+                          <span>{`Quantity: ${order.products[magnet.itemNumber]}`}</span>
+                        </div>
+                        <div>
+                          <h5>{`Subtotal: $${(+magnet.price * order.products[magnet.itemNumber]).toFixed(2)}`}</h5>
+                        </div>
+                      </div>
+                      )
+                    })
+                  }
+                  <h4>{`Total: $${order.subtotal}`}</h4>
+                </div>
+              </li>
+              </div>
+            )
+          })
+        }
+      </ul>
     </div>
   )
 }
