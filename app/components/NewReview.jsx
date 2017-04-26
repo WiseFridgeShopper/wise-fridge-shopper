fcb35de194d13b8f72e8f70b9bdf93b3a1eb0ffb
Anton Cataldi <antoncataldi@gmail.com>
2017-04-25 20:04:19 -0400

    add change/submit handlers to NewReview component

--- app/components/NewReview.jsx
+++ app/components/NewReview.jsx
@@ -7,11 +7,44 @@ import MenuItem from 'material-ui/MenuItem'
 import DropDownMenu from 'material-ui/DropDownMenu'
 
 export default class NewReview extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      magnetToReview: {},
+      rating: null,
+      comment: ''
+    }
+    this.onMagnetChange = this.onMagnetChange.bind(this)
+    this.onCommentChange = this.onCommentChange.bind(this)
+    this.onRatingChange = this.onRatingChange.bind(this)
+    this.onSubmit = this.onSubmit.bind(this)
+  }
+
+  onMagnetChange(evt) {
+    this.setState({
+      magnetToReview: evt.target.value
+    })
+  }
+
+  onCommentChange(evt) {
+    this.setState({
+      comment: evt.target.value
+    })
+  }
+  onRatingChange(evt) {
+    this.setState({
+      rating: evt.target.value
+    })
+  }
+  onSubmit(evt) {
+    evt.preventDefault()
+
+  }
   render() {
-    const magnetsToReview = [1, 2, 3]
+    const magnetsToReview = [{id: 1, text: 'mag1'}, {id: 2, text: 'mag2'}, {id: 3, text: 'mag3'}]
     return (
       <form>
-       <DropDownMenu value={''} onChange={this.handleChange}>
+       <DropDownMenu value={this.state.magnetToReview} onChange={this.handleChange}>
          {magnetsToReview && magnetsToReview.map(magnet => {
            return <MenuItem value={magnet.id} primaryText={magnet} />
          })}
@@ -19,7 +52,7 @@ export default class NewReview extends React.Component {
        <br />
        <TextField ref="login" type="text" name="login" floatingLabelText="Insert Comment Here"/>
        <br />
-       <TextField ref="password" type="password" name="password" floatingLabelText="Rating (Number between 1-5)"/>
+       <TextField ref="password" type="number" name="password" floatingLabelText="Rating (Number between 1-5)"/>
        <br />
         <RaisedButton type="submit" label="Create Review" className="button-submit" primary={true} />
       </form>
